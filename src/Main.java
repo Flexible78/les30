97b5a17a47c2e9518aa0a26a84d1ad9c1061b249
ALEXANDER <120051629+Flexible78@users.noreply.github.com>
2025-11-20 19:42:12 +0200

    Remove duplicate print and add new message
    Removed duplicate 'bye!bye!' print statement and added a new print statement.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     static void main() {
         System.out.println("hello!");
-        System.out.println("bye!bye!");
+        System.out.println("bye!пппbye!");
         System.out.println("bye!bye!");
 
     }
